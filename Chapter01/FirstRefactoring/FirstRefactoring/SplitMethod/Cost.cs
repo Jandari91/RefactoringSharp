76377b714235f1c1da0a8f7a7e623c4d529959fc
bak <pys02016478@gmail.com>
2022-06-02 23:52:36 +0900

    Chapter1 작성중

---
+++ Chapter01/FirstRefactoring/FirstRefactoring/SplitMethod/Cost.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Immutable;
+
+
+namespace FirstRefactoring.SplitMethod;
+
+public class Cost
+{
+
+    public Cost()
+    {
+        
+        var invoice = JsonConvert.DeserializeObject<Invoice>(ReadJsonFile("SplitMethod/invoices.json"));
+        var plays = JsonConvert.DeserializeObject<IImmutableDictionary<string, Play>>(ReadJsonFile("SplitMethod/plays.json"));
+    }
+
+    public string Statement(Invoice invoice, IImmutableDictionary<string, Play> plays)
+    {
+        return "";
+    }
+
+    private string ReadJsonFile(string filePath)
+    {
+        StreamReader r = new StreamReader(filePath);
+        string jsonString = r.ReadToEnd();
+        return jsonString;
+    }
+}
+
+
+public class Play
+{
+    public string Name { get; }
+    public string Type { get; }
+
+    public Play(string name, string payType)
+    {
+        Name = name;
+        Type = payType;
+    }
+}
+
+public class Performance
+{
+    public  int Audience { get; }
+    public  string PlayId { get; }
+
+    public Performance(string playId, int audience)
+    {
+        PlayId = playId;
+        Audience = audience;
+    }
+}
+
+public class Invoice
+{
+    public string Customer { get; }
+    public ImmutableList<Performance> Performances { get; }
+        
+    public Invoice(string customer, ImmutableList<Performance> performances)
+    {
+        Customer = customer;
+        Performances = performances;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# RefactoringSharp
+# REFACTORING
+
+![](./images/1.jpg)
+
 마틴 파울러의 리팩토링을 C# 버전으로 적는 레파지토리입니다.
+
+## Enviroment
+* .NET 6
+
+## Build
+* Visual Studio 2022
+
+## 참고
+
+* https://github.com/Wegralee/Refactoring
